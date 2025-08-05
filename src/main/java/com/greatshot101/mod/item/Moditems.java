fe8d70a2501e39692514b2c3a8e4b8d48e73629d
greatshot101 <greatshot101@google.com>
2025-08-05 03:32:53 -0500

    first commit

---
+++ src/main/java/com/greatshot101/mod/item/Moditems.java
@@ -0,0 +1,28 @@
+package com.greatshot101.mod.item;
+
+
+
+import com.greatshot101.mod.mod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+
+public class Moditems {
+    public static final DeferredRegister<Item> ITEMS =
+        DeferredRegister.create(ForgeRegistries.ITEMS,mod.MODID);
+
+
+
+
+    public static final RegistryObject<Item> Titanium = ITEMS.register("Titanium",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
